675ca51a8940da748b4c6ecbc93304b2102a1abe
Kriksson <150140637+Kriksson@users.noreply.github.com>
2025-04-24 11:42:37 +0300

    Add files via upload

---
+++ usbms.lua
@@ -0,0 +1,8 @@
+function main()
+    while not isSampAvailable() do wait(0) end
+        sampAddChatMessage('USBMS.LUA -> щрю бепяхъ яйпхорю анкэье ме онддепфхбюеряъ!', -1)
+    while true do
+        wait(0)
+        
+    end
+end
