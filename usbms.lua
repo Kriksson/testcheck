f1261f316a4795d2930547798e94970c48cd64c3
Kriksson <150140637+Kriksson@users.noreply.github.com>
2025-11-18 21:50:51 +0300

    Delete usbms.lua

--- usbms.lua
+++
@@ -1,8 +0,0 @@
-function main()
-    while not isSampAvailable() do wait(0) end
-        sampAddChatMessage('USBMS.LUA -> щрю бепяхъ яйпхорю анкэье ме онддепфхбюеряъ!', -1)
-    while true do
-        wait(0)
-        
-    end
-end
